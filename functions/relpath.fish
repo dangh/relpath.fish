a2df7d3d74b21b6d34a7ee80d1bb9d08fab3c3a0
Dang <huynhminhdang@gmail.com>
2022-09-19 10:00:24 +0700

    use short flags

--- functions/relpath.fish
+++ functions/relpath.fish
@@ -1,10 +1,10 @@
-function relpath --argument-names source target --description "Print target path with relative to the source path"
+function relpath -a source target -d "Print target path with relative to the source path"
   set source (builtin realpath $source)
   set target (builtin realpath $target)
-  set --local base
+  set -l base
   while test -n "$source" -a -n "$target"
-    echo $source | read --local --delimiter=/ source_base source_rest
-    echo $target | read --local --delimiter=/ target_base target_rest
+    echo $source | read -l -d / source_base source_rest
+    echo $target | read -l -d / target_base target_rest
     test "$source_base" = "$target_base" || break
     set base "$base$source_base/"
     set source "$source_rest"
@@ -13,7 +13,7 @@ function relpath --argument-names source target --description "Print target path
   if test -z "$source"
     set source "."
   else
-    set source (string replace --all --regex '[^/]+' '..' -- $source)
+    set source (string replace -a -r '[^/]+' '..' -- $source)
   end
-  string replace --regex '/$' '' -- "$source/$target"
+  string replace -r '/$' '' -- "$source/$target"
 end
